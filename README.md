6ee55cc5c18505519909c01d4d6cf7f39dda9e9f
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2025-03-02 12:54:31 -0300

    feat: update

---
+++ README.md
@@ -0,0 +1,21 @@
+# Chatbot - GPT
+
+### Pre-requisites
+Environment:
+ - OPENAI_API_KEY
+
+### Create a virtual environment
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+### Installation
+```bash
+pip3 install -r requirements.txt
+```
+
+### How to run
+```bash
+python3 chatbot.py
+```
---
+++ chatbot.py
@@ -0,0 +1,40 @@
+import openai
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+
+client = openai.Client()
+
+def send_message(chat):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=chat,
+        temperature=0,
+        max_tokens=1000,
+        stream=True,
+    )
+    print('GPT: ', end='')
+    message_completed = ''
+    for stream in response:
+        message = stream.choices[0].delta.content
+        if message:
+            print(message, end='')
+            message_completed += message
+
+    print()
+
+    chat.append({
+        'role': 'assistant',
+        'content': message_completed
+    })
+    return chat
+
+if __name__ == '__main__':
+    print('Starting Chat GPT')
+    messages = []
+    while True:
+        user_message = input('Nando: ')
+        messages.append({'role': 'user', 'content': user_message})
+        messages = send_message(messages)
+
+
